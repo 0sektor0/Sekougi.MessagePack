ba01b6443d0e3fb53dd5f02f91da613c831244e2
0sekto0 <tomskiy_a@list.ru>
2020-03-09 23:07:03 +0300

    benchmark initialization added and added string benchmark

--- Src/Sekougi.MessagePack.Benchmarks/SerializationBenchmark.cs
+++ Src/Sekougi.MessagePack.Benchmarks/SerializationBenchmark.cs
@@ -1,8 +1,8 @@
-using System.IO;
-using BenchmarkDotNet.Attributes;
-using MsgPack;
-using MsgPack.Serialization;
 using Sekougi.MessagePack.Serializers;
+using BenchmarkDotNet.Attributes;
+using MsgPack.Serialization;
+using System.IO;
+using MsgPack;
 
 
 
@@ -11,32 +11,67 @@ namespace Sekougi.MessagePack.Benchmarks
     [MemoryDiagnoser]
     public class SerializationBenchmark
     {
-        private readonly Packer _packerCli;
-        private readonly MsgPack.Serialization.MessagePackSerializer<int> _serializerIntCli;
-        private readonly MsgPack.Serialization.MessagePackSerializer<double> _serializerDoubleCli;
+        private string _shortString;
+        private string _averageString;
+        private string _largeString;
         
-        private readonly MessagePackWriter _writerSekougi;
-        private readonly Serializers.MessagePackSerializer<int> _serializerIntSekougi;
-        private readonly Serializers.MessagePackSerializer<double> _serializerDoubleSekougi;
+        private Stream _cliStream;
+        private Packer _packerCli;
+        private MsgPack.Serialization.MessagePackSerializer<int> _serializerIntCli;
+        private MsgPack.Serialization.MessagePackSerializer<double> _serializerDoubleCli;
+        private MsgPack.Serialization.MessagePackSerializer<float> _serializerFloatCli;
+        private MsgPack.Serialization.MessagePackSerializer<string> _serializerStringCli;
+
+        private IMessagePackBuffer _sekougiBuffer;
+        private MessagePackWriter _writerSekougi;
+        private Serializers.MessagePackSerializer<int> _serializerIntSekougi;
+        private Serializers.MessagePackSerializer<double> _serializerDoubleSekougi;
+        private Serializers.MessagePackSerializer<float> _serializerFloatSekougi;
+        private Serializers.MessagePackSerializer<string> _serializerStringSekougi;
 
         
-        public SerializationBenchmark()
+        [GlobalSetup]
+        public void Setup()
         {
-            var cliStream = new MemoryStream();
-            _packerCli = Packer.Create(cliStream);            
+            _shortString = new string(new char[10]);
+            _averageString = new string(new char[10000]);
+            _largeString = new string(new char[int.MaxValue / 10]);
+            
             _serializerIntCli =  MessagePackSerializer.Get<int>();
             _serializerDoubleCli = MessagePackSerializer.Get<double>();
-
-            var sekougiBuffer = new MessagePackStreamBuffer();
-            _writerSekougi = new MessagePackWriter(sekougiBuffer);
+            _serializerFloatCli = MessagePackSerializer.Get<float>();
+            _serializerStringCli = MessagePackSerializer.Get<string>();
+            
             _serializerIntSekougi = MessagePackSerializersReposetory.Get<int>();
             _serializerDoubleSekougi = MessagePackSerializersReposetory.Get<double>();
+            _serializerFloatSekougi = MessagePackSerializersReposetory.Get<float>();
+            _serializerStringSekougi = MessagePackSerializersReposetory.Get<string>();
+        }
+        
+        [IterationSetup]
+        public void IterationSetup()
+        {
+            const int BUFFER_CAPACITY = 4096;
+            
+            _cliStream = new MemoryStream(BUFFER_CAPACITY);
+            _packerCli = Packer.Create(_cliStream);            
+
+            _sekougiBuffer = new MessagePackStreamBuffer(BUFFER_CAPACITY);
+            _writerSekougi = new MessagePackWriter(_sekougiBuffer);
         }
 
+        [IterationCleanup]
+        public void IterationCleanup()
+        {
+            _cliStream.Dispose();
+            _sekougiBuffer.Dispose();
+        }
 
         [Benchmark]
         [Arguments(1, int.MaxValue)]
         [Arguments(100, int.MaxValue)]
+        [Arguments(100, 255)]
+        [Arguments(100, 256)]
         public void SerializeIntCli(int count, int value)
         {
             for (var i = 0; i < count; i++)
@@ -48,6 +83,8 @@ namespace Sekougi.MessagePack.Benchmarks
         [Benchmark]
         [Arguments(1, int.MaxValue)]
         [Arguments(100, int.MaxValue)]
+        [Arguments(100, 255)]
+        [Arguments(100, 256)]
         public void SerializeIntSekougi(int count, int value)
         {
             for (var i = 0; i < count; i++)
@@ -59,6 +96,7 @@ namespace Sekougi.MessagePack.Benchmarks
         [Benchmark]
         [Arguments(1, double.MaxValue)]
         [Arguments(100, double.MaxValue)]
+        [Arguments(100, 255.1d)]
         public void SerializeDoubleCli(int count, double value)
         {
             for (var i = 0; i < count; i++)
@@ -70,6 +108,7 @@ namespace Sekougi.MessagePack.Benchmarks
         [Benchmark]
         [Arguments(1, double.MaxValue)]
         [Arguments(100, double.MaxValue)]
+        [Arguments(100, 255.1d)]
         public void SerializeDoubleSekougi(int count, double value)
         {
             for (var i = 0; i < count; i++)
@@ -77,5 +116,47 @@ namespace Sekougi.MessagePack.Benchmarks
                 _serializerDoubleSekougi.Serialize(value, _writerSekougi);
             }
         }
+        
+        [Benchmark]
+        [Arguments(1, float.MaxValue)]
+        [Arguments(100, float.MaxValue)]
+        [Arguments(100, 255.1f)]
+        public void SerializeFloatCli(int count, float value)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                _serializerFloatCli.PackTo(_packerCli, value);
+            }
+        }
+        
+        [Benchmark]
+        [Arguments(1, float.MaxValue)]
+        [Arguments(100, float.MaxValue)]
+        [Arguments(100, 255.1f)]
+        public void SerializeFloatSekougi(int count, float value)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                _serializerFloatSekougi.Serialize(value, _writerSekougi);
+            }
+        }
+        
+        [Benchmark]
+        public void SerializeShortStringCli() => _serializerStringCli.PackTo(_packerCli, _shortString);
+        
+        [Benchmark]
+        public void SerializeAverageStringCli() => _serializerStringCli.PackTo(_packerCli, _averageString);
+        
+        [Benchmark]
+        public void SerializeLargeStringCli() => _serializerStringCli.PackTo(_packerCli, _largeString);
+        
+        [Benchmark]
+        public void SerializeShortStringSekougi() => _serializerStringSekougi.Serialize(_shortString, _writerSekougi);
+        
+        [Benchmark]
+        public void SerializeAverageStringSekougi() => _serializerStringSekougi.Serialize(_averageString, _writerSekougi);
+        
+        [Benchmark]
+        public void SerializeLargeStringSekougi() => _serializerStringSekougi.Serialize(_largeString, _writerSekougi);
     }
 }
