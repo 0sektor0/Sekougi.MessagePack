4c98f25afdd17fea3294a1c189beebc07fcad37d
0sekto0 <tomskiy_a@list.ru>
2020-03-09 23:23:50 +0300

    Compatibility tests started

---
+++ Src/Sekougi.MessagePack.Tests/MessagePackCompatibilityTest.cs
@@ -0,0 +1,48 @@
+using MsgPack;
+using MsgPack.Serialization;
+using Sekougi.MessagePack.Serializers;
+using Xunit;
+
+
+
+namespace Sekougi.MessagePack.Tests
+{
+    public class MessagePackCompatibilityTest
+    {
+        [Fact]
+        public void IntegerNumbersCompatibilityTest()
+        {
+            using var buffer = new MessagePackStreamBuffer();
+            using var packer = Packer.Create(buffer);
+            
+            var cliSerializer = MessagePackSerializer.Get<long>();
+            var sekougiSerializer = MessagePackSerializersReposetory.Get<long>();
+
+            var values = new[]
+            {
+                0,
+                -1,
+                32,
+                sbyte.MaxValue,
+                sbyte.MinValue,
+                short.MaxValue,
+                short.MinValue,
+                int.MaxValue,
+                int.MinValue,
+                long.MaxValue,
+                long.MinValue,
+            };
+
+            foreach (var value in values)
+            {
+                cliSerializer.PackTo(packer, value);
+            }
+
+            buffer.Drop();
+            foreach (var value in values)
+            {
+                Assert.Equal(value, sekougiSerializer.Deserialize(buffer));
+            }
+        }
+    }
+}
