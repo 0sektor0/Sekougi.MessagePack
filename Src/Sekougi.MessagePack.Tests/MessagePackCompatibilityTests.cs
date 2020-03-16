a0b5bd5f80b148d8ae2dc69d0ef380327d6379fb
0sektor0 <tomskiy_a@list.ru>
2020-03-16 10:50:06 +0300

    compatibility test template function added

--- Src/Sekougi.MessagePack.Tests/MessagePackCompatibilityTests.cs
+++ Src/Sekougi.MessagePack.Tests/MessagePackCompatibilityTests.cs
@@ -10,14 +10,20 @@ namespace Sekougi.MessagePack.Tests
     public class MessagePackCompatibilityTests
     {
         [Fact]
+        public void BoolCompatibilityTest()
+        {
+            var values = new[]
+            {
+                true,
+                false,
+            };
+            
+            CompatibilityTest(values);
+        }
+        
+        [Fact]
         public void IntegerNumbersCompatibilityTest()
         {
-            using var buffer = new MessagePackStreamBuffer();
-            using var packer = Packer.Create(buffer);
-            
-            var cliSerializer = MessagePackSerializer.Get<long>();
-            var sekougiSerializer = MessagePackSerializersReposetory.Get<long>();
-
             var values = new[]
             {
                 0,
@@ -32,7 +38,41 @@ namespace Sekougi.MessagePack.Tests
                 long.MaxValue,
                 long.MinValue,
             };
+            
+            CompatibilityTest(values);
+        }
 
+        [Fact]
+        public void FloatCompabilityTest()
+        {
+            var values = new[]
+            {
+                0,
+                -1f,
+                1f,
+                -1.1f,
+                1.1f,
+                sbyte.MaxValue,
+                sbyte.MinValue,
+                short.MaxValue,
+                short.MinValue,
+                int.MaxValue,
+                int.MinValue,
+                long.MaxValue,
+                long.MinValue,
+            };
+            
+            CompatibilityTest(values);
+        }
+
+        private void CompatibilityTest<T>(T[] values)
+        {
+            using var buffer = new MessagePackStreamBuffer();
+            using var packer = Packer.Create(buffer);
+            
+            var cliSerializer = MessagePackSerializer.Get<T>();
+            var sekougiSerializer = MessagePackSerializersReposetory.Get<T>();
+            
             foreach (var value in values)
             {
                 cliSerializer.PackTo(packer, value);
